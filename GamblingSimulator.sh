650426300df2cfd3b11751386a196fbba53557dd
mohammed92-max <shahrukhkhandb@gmail.com>
2020-06-09 00:37:11 +0530

    Added shell file

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -6,4 +6,16 @@ betEveryGame=1;
 echo "Gambler starts with stake of $everyDayStake$ everyday"
 echo "Gambler bets $betEveryGame$ every game"
 
+gamblerCheck=$((RANDOM%2));
+LOOSE=0;
+WIN=1;
 
+if [[ $gamblerCheck -eq LOOSE ]]
+then
+        echo "Gambler loose $1"
+fi
+
+if [[ $gamblerCheck -eq WIN ]]
+then
+        echo "Gambler won $1"
+fi
