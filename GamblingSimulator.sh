30bd436e022ea87a605ebeb69efa31d303eb8b85
mohammed92-max <shahrukhkhandb@gmail.com>
2020-06-09 05:13:22 +0530

    added shell file

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -6,16 +6,39 @@ betEveryGame=1;
 echo "Gambler starts with stake of $everyDayStake$ everyday"
 echo "Gambler bets $betEveryGame$ every game"
 
-gamblerCheck=$((RANDOM%2));
-LOOSE=0;
 WIN=1;
+LOOSE=0;
+win=0;
+loose=0;
+bet=0;
 
-if [[ $gamblerCheck -eq LOOSE ]]
-then
-        echo "Gambler loose $1"
-fi
+while(($bet <= 100))
+do
+        ((bet++));
+        gamblerCheck=$((RANDOM%2));
 
-if [[ $gamblerCheck -eq WIN ]]
-then
-        echo "Gambler won $1"
-fi
+        if [[ $gamblerCheck -eq WIN ]]
+        then
+                echo "Gambler won 1$"
+                win=$(($win+1));
+        fi
+
+        if [[ $gamblerCheck -eq LOOSE ]]
+        then
+                echo "Gambler loose 1$"
+                loose=$(($loose+1));
+        fi
+
+        if [[ $win == 50 ]]
+        then
+                echo "Gambler won 50 times & resigned for the day"
+                break;
+        fi
+
+        if [[ $loose == 50 ]]
+        then
+                echo "Gambler loose 50 times & resigned for the day"
+                break;
+        fi
+done
+
