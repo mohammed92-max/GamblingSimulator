45090f75eea0aa30bb097fdfcc84c29a954fe4f1
mohammed92-max <shahrukhkhandb@gmail.com>
2020-06-09 05:21:58 +0530

    added shell file

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -8,37 +8,54 @@ echo "Gambler bets $betEveryGame$ every game"
 
 WIN=1;
 LOOSE=0;
-win=0;
-loose=0;
-bet=0;
+totalAmountWon=0;
+totalAmountLost=0;
 
-while(($bet <= 100))
+for((day=1; day<=20; day++))
 do
+        bet=1;
+        win=0;
+    loose=0;
+
+        while(($bet <= 100))
+        do
         ((bet++));
         gamblerCheck=$((RANDOM%2));
 
         if [[ $gamblerCheck -eq WIN ]]
         then
-                echo "Gambler won 1$"
+                echo "Gambler has been won 1$"
                 win=$(($win+1));
         fi
 
         if [[ $gamblerCheck -eq LOOSE ]]
         then
-                echo "Gambler loose 1$"
-                loose=$(($loose+1));
+            echo "Gambler has loose 1$"
+            loose=$(($loose+1));
         fi
 
-        if [[ $win == 50 ]]
-        then
-                echo "Gambler won 50 times & resigned for the day"
-                break;
-        fi
+            if [[ $win == 50 ]]
+                then
+            echo "Gambler won 50 times & resigned for the day"
+                    break;
+            fi
 
-        if [[ $loose == 50 ]]
-        then
+            if [[ $loose == 50 ]]
+            then
                 echo "Gambler loose 50 times & resigned for the day"
-                break;
-        fi
+                        break;
+                fi
+
+                echo "Amount won per day is $win$";
+                echo "Amount lost per day is $loose$";
+        done
+
+        totalAmountWon=$(($totalAmountWon+$win));
+        totalAmountLost=$(($totalAmountLost+$loose));
 done
+
+echo "Total amount won in 20 days is $totalAmountWon$"
+echo "Total amount lost in 20 days is $totalAmountLost$"
+
+
 
